b28b3f8be5a3e019fcea0cb7cfa88243ed8032bf
sgali31 <sgali3@uncc.edu>
2025-02-09 18:31:54 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
-// Budget API
-
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(cors());
-app.use('/budget', express.static('public'));
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+// Use CORS if needed
+// app.use(cors());
 
+
+app.use('/', express.static('public'));
+
+
+app.get('/hello', (req, res) => {
+    res.send('Hello world');
+});
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    
+    fs.readFile(path.join(__dirname, 'budget-data.json'), 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).json({ error: 'Failed to read budget data' });
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
 });
+
 
 app.listen(port, () => {
     console.log(`API served at http://localhost:${port}`);
